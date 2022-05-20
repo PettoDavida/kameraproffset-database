17e948ce65f27d135db00983ffd5dffebe46960c
PettoDavida <maximilian.widman@gmail.com>
2022-05-20 10:11:42 +0200

    created orderController functions

---
+++ backend/order/controllers/orderController.ts
@@ -0,0 +1,33 @@
+import { NextFunction, Request, Response } from "express";
+
+
+const getAllOrders  = async (req: Request, res: Response) => {
+    
+}
+
+const getOrderByID = async (req: Request, res: Response) => {
+
+}
+
+const getOrderByUserID = async (req: Request, res: Response) => {
+    
+}
+
+const addOrder = async (req: Request, res: Response) => {
+    
+}
+
+const setOrderToSent = async (req: Request, res: Response) => {
+    
+}
+
+const deleteOrder = async (req: Request, res: Response) => {
+    
+}
+
+export{getAllOrders, 
+       getOrderByID, 
+       getOrderByUserID, 
+       addOrder, 
+       setOrderToSent, 
+       deleteOrder}
