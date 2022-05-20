8d62fbac7b96711adcbdf93d3d63239b0de71dce
PettoDavida <maximilian.widman@gmail.com>
2022-05-20 10:12:12 +0200

    added Order endpoints

--- backend/order/routes/orderRoutes.ts
+++ backend/order/routes/orderRoutes.ts
@@ -1,5 +1,15 @@
 import express from 'express';
-let router = express.Router();
+import { getAllOrders, getOrderByID, getOrderByUserID, addOrder, setOrderToSent, deleteOrder } from '../controllers/orderController'
 
 
-export default router;
+let orderRouter = express
+                 .Router()
+                 .get('/order', getAllOrders)
+                 .get('/order/:id', getOrderByID)
+                 .get('/order/:userID', getOrderByUserID)
+                 .post('/order', addOrder)
+                 .put('/order/:id', setOrderToSent)
+                 .delete('/order/:id', deleteOrder)
+
+
+export default orderRouter;
