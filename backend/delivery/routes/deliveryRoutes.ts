1204f9416f7f5de454b5ca6049a5aa760180dbb7
AdrianRydin <adrian.rydin@gmail.com>
2022-05-20 09:56:40 +0200

    Fixed delivery

--- backend/delivery/routes/deliveryRoutes.ts
+++ backend/delivery/routes/deliveryRoutes.ts
@@ -1,5 +1,12 @@
 import express from 'express';
-let router = express.Router();
+import { getAllDeliveryMethods, addDeliveryMethod, editDeliveryMethod, deleteDeliveryMethod } from '../controllers/deliveryController'
+
+let deliveryRouter = express
+        .Router()
+        .get('/delivery', getAllDeliveryMethods)
+        .post('/delivery', addDeliveryMethod)
+        .put('/delivery/:id', editDeliveryMethod)
+        .delete('/delivery/:id', deleteDeliveryMethod)
 
 
-export default router;
+export default deliveryRouter;
