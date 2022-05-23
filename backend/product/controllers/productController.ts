0fccc7d384edef266c28394ce18a00fe40c4583b
AdrianRydin <adrian.rydin@gmail.com>
2022-05-23 10:14:30 +0200

    rest + controller

--- backend/product/controllers/productController.ts
+++ backend/product/controllers/productController.ts
@@ -1,6 +1,15 @@
 import { NextFunction, Request, Response } from "express";
+import { productModel } from "../models/productModels";
 
-export const getAllProducts = async (req: Request, res: Response) => {};
+export const getAllProducts = async (req: Request, res: Response, next: NextFunction) => {
+    try{
+        const allProducts = await productModel.find({});
+        res.status(200).json(allProducts);
+    }catch (err) {
+        res.status(404).json("No products were found");
+        next(err);
+    }
+};
 
 export const getProduct = async (req: Request, res: Response) => {};
 
