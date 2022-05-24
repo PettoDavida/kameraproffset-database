306adab98b2e274fe048dfd45527d85673163a72
PettoDavida <maximilian.widman@gmail.com>
2022-05-24 15:31:35 +0200

    initialised cors

--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,7 @@
 import * as dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 
 import userRouter from "./user/routes/userRoutes";
 import deliveryRouter from "./delivery/routes/deliveryRoutes";
@@ -8,12 +9,12 @@ import orderRouter from "./order/routes/orderRoutes";
 import productRouter from "./product/routes/productRoutes";
 import { categoryRouter } from "./category/routes/categoryRoutes";
 
-
 dotenv.config({ path: __dirname + "/.env" });
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(express.json());
 
 function err(message: string) {
