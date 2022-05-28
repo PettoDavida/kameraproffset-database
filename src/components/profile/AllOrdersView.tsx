c8b8668f2d8f3ffa74a276bde75c559ba108622c
PettoDavida <maximilian.widman@gmail.com>
2022-05-29 00:35:04 +0200

    started work on getting all orders and displaying on admin side of profile page

--- src/components/profile/AllOrdersView.tsx
+++ src/components/profile/AllOrdersView.tsx
@@ -1,3 +1,32 @@
+import { useState } from "react";
+
+interface Orders {
+  userID: string;
+}
+
 export default function AllOrdersView() {
+  const [orders, setOrders] = useState<any[]>([]);
+  let headers: RequestInit = {
+    method: "GET",
+  };
+  fetch("http://localhost:3000/api/order", headers)
+    .then((res: Response) => {
+      if (res.status === 500) {
+        return Promise.reject("Internal server error");
+      } else if (res.status === 400) {
+        return Promise.reject("oldPassword and newPassword required");
+      }
+
+      return res.json();
+    })
+    .then((data) => {
+      setOrders(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  console.log(orders);
+
   return <div></div>;
 }
