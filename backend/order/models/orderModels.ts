4fa0a7863314ec0d7ed9978f7d72bd8c9a8fbc17
PettoDavida <maximilian.widman@gmail.com>
2022-05-29 00:36:04 +0200

    changed types to correct types and changed schema to use other schemas as values

--- backend/order/models/orderModels.ts
+++ backend/order/models/orderModels.ts
@@ -1,28 +1,27 @@
-import mongoose from 'mongoose'
-import { Address, AddressModel }from '../../common'
+import mongoose from "mongoose";
+import { Address, addressSchema } from "../../common";
+import { Delivery, deliverySchema } from "../../delivery/models/deliveryModel";
+import { Product, productSchema } from "../../product/models/productModels";
 
 export interface Order {
-    products: []
-    deliveryAddress: Address
-    deliveryOption: String
-    sent: Boolean
+  products: Product[];
+  deliveryAddress: Address;
+  deliveryOption: Delivery;
+  sent: Boolean;
 }
 
 const orderSchema = new mongoose.Schema<Order>(
-    {
-        products: { type: [], required: true },
-        deliveryAddress: { type: AddressModel, required: true },
-        deliveryOption: { type: String, required: true },
-        sent: { type: Boolean, required: true, default: false },
-    },
-    {
-        timestamps: { createdAt: true, updatedAt: false },
-        toJSON: { virtuals: true },
-        toObject: { virtuals: true }
-    }
-)
+  {
+    products: { type: [productSchema], required: true },
+    deliveryAddress: { type: addressSchema, required: true },
+    deliveryOption: { type: deliverySchema, required: true },
+    sent: { type: Boolean, required: true, default: false },
+  },
+  {
+    timestamps: { createdAt: true, updatedAt: false },
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
 
-
-
-
-export const OrderModel = mongoose.model('order', orderSchema)
+export const OrderModel = mongoose.model("order", orderSchema);
