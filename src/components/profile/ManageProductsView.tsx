d8b3edae29fe2b0a61fdb759937c56eb61cf0404
PettoDavida <maximilian.widman@gmail.com>
2022-05-29 19:38:39 +0200

    started work on edit product and removed theme

--- src/components/profile/ManageProductsView.tsx
+++ src/components/profile/ManageProductsView.tsx
@@ -6,13 +6,11 @@ import {
   AccordionSummary,
   Button,
   ButtonGroup,
-  createTheme,
   Dialog,
   DialogActions,
   DialogContent,
   DialogContentText,
   DialogTitle,
-  ThemeProvider,
   Typography,
 } from "@mui/material";
 import { useEffect, useState } from "react";
@@ -21,6 +19,7 @@ import AddIcon from "@mui/icons-material/Add";
 import ExpandMore from "@mui/icons-material/ExpandMore";
 import NewProductForm from "./NewProductForm";
 import { Close } from "@mui/icons-material";
+import EditProductForm from "./EditProductForm";
 
 interface Product {
   _id: String;
@@ -32,22 +31,6 @@ interface Product {
   category: String[];
   stock?: number;
 }
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#333333",
-      contrastText: "#FBF7F5", //button text white instead of black
-    },
-    background: {
-      default: "#333333",
-    },
-
-    secondary: {
-      main: "#DA344D",
-    },
-  },
-});
 
 function ManageProductsView() {
   const [products, setProducts] = useState<Product[]>([]);
@@ -78,77 +61,85 @@ function ManageProductsView() {
   }, []);
 
   return (
-    <ThemeProvider theme={theme}>
-      <div className="admin-top-container">
-        <div className="admin-edit-button">
-          <Button
-            onClick={handleClickOpen}
-            variant="contained"
-            size="large"
-            color="primary"
-          >
-            <AddIcon /> Lägg till produkt
-          </Button>
-          <Dialog open={open} onClose={handleClose}>
-            <DialogActions>
-              <Button onClick={handleClose}>
-                <Close />
-              </Button>
-            </DialogActions>
-            <DialogTitle>Create new product</DialogTitle>
-            <DialogContent>
-              <DialogContentText>
-                If a field has * it's required
-              </DialogContentText>
-              <NewProductForm />
-            </DialogContent>
-          </Dialog>
-        </div>
-        <div className="admin-container">
-          {products.map((item: Product, i: number) => (
-            <Accordion key={i} sx={{ width: "100%" }}>
-              <AccordionSummary
-                expandIcon={<ExpandMore />}
-                aria-controls="panel1a-content"
-                id="panel1a-header"
-              >
-                <Typography>{item.title}</Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Typography>{item.longInfo}</Typography>
-                <ButtonGroup>
-                  <Button
-                    variant="contained"
-                    onClick={() => console.log(item)}
-                    startIcon={<EditIcon />}
-                  >
-                    Edit
-                  </Button>
-                  <Button
-                    color="secondary"
-                    variant="contained"
-                    onClick={() => {
-                      let headers: RequestInit = {
-                        method: "DELETE",
-                      };
-                      fetch(
-                        `http://localhost:3000/api/products/${item._id}`,
-                        headers
-                      ).then(() => {
-                        getProductFromBackend();
-                      });
-                    }}
-                    startIcon={<DeleteForeverIcon />}
-                  >
-                    Remove
-                  </Button>
-                </ButtonGroup>
-              </AccordionDetails>
-            </Accordion>
-          ))}
-        </div>
+    <div className="admin-top-container">
+      <div className="admin-edit-button">
+        <Button onClick={handleClickOpen} variant="contained" size="large">
+          <AddIcon /> Lägg till produkt
+        </Button>
+        <Dialog open={open} onClose={handleClose}>
+          <DialogActions>
+            <Button onClick={handleClose}>
+              <Close />
+            </Button>
+          </DialogActions>
+          <DialogTitle>Create new product</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              If a field has * it's required
+            </DialogContentText>
+            <NewProductForm />
+          </DialogContent>
+        </Dialog>
       </div>
-    </ThemeProvider>
+      <div className="admin-container">
+        {products.map((item: Product, i: number) => (
+          <Accordion key={i} sx={{ width: "100%" }}>
+            <AccordionSummary
+              expandIcon={<ExpandMore />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography>{item.title}</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>{item.longInfo}</Typography>
+              <ButtonGroup>
+                <Button
+                  onClick={handleClickOpen}
+                  variant="contained"
+                  size="large"
+                  startIcon={<EditIcon />}
+                >
+                  Edit
+                </Button>
+                <Dialog open={open} onClose={handleClose}>
+                  <DialogActions>
+                    <Button onClick={handleClose}>
+                      <Close />
+                    </Button>
+                  </DialogActions>
+                  <DialogTitle>Edit product {item.title}</DialogTitle>
+                  <DialogContent>
+                    <DialogContentText>
+                      If a field has * it's required
+                    </DialogContentText>
+                    <EditProductForm {...item} />
+                  </DialogContent>
+                </Dialog>
+                <Button
+                  color="error"
+                  variant="contained"
+                  onClick={() => {
+                    let headers: RequestInit = {
+                      method: "DELETE",
+                    };
+                    fetch(
+                      `http://localhost:3000/api/products/${item._id}`,
+                      headers
+                    ).then(() => {
+                      getProductFromBackend();
+                    });
+                  }}
+                  startIcon={<DeleteForeverIcon />}
+                >
+                  Remove
+                </Button>
+              </ButtonGroup>
+            </AccordionDetails>
+          </Accordion>
+        ))}
+      </div>
+    </div>
   );
 }
 
