8bdb3c559e71b58b839230e8cffdc636b9ba816e
PettoDavida <maximilian.widman@gmail.com>
2022-06-01 11:05:46 +0200

    getAll

--- backend/media/routes/mediaRouter.ts
+++ backend/media/routes/mediaRouter.ts
@@ -4,12 +4,14 @@ import {
   getMedia,
   addMedia,
   deleteMedia,
+  getAllMedia,
 } from "../controllers/mediaController";
 
 const upload = multer();
 
 export const mediaRouter = express
   .Router()
+  .get("/media", getAllMedia)
   .get("/media/:id", getMedia)
   .post("/media", upload.single("media"), addMedia)
   .delete("/media/:id", deleteMedia);
