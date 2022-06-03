2b7e62b5cb8866fd09c5aa5e7912a6469fd37344
PettoDavida <maximilian.widman@gmail.com>
2022-06-03 15:05:47 +0200

    Styling and displaying correct info when viewing product from admin perspective

--- src/components/profile/ManageProductsView.tsx
+++ src/components/profile/ManageProductsView.tsx
@@ -20,12 +20,21 @@ import ExpandMore from "@mui/icons-material/ExpandMore";
 import NewProductForm from "./NewProductForm";
 import { Close } from "@mui/icons-material";
 import EditProductForm from "./EditProductForm";
-import { ProductBackend } from "../../utils/backend";
+import {
+  CategoryBackend,
+  getCategoriesFromBackend,
+  getImageUrl,
+  ProductBackend,
+  Specs,
+} from "../../utils/backend";
 import { getLoginToken } from "../../utils/token";
+import "./CSS/ManageProductsView.css";
 
 function ManageProductsView() {
   const [products, setProducts] = useState<ProductBackend[]>([]);
   const [open, setOpen] = useState<String>("");
+
+  const [categories, setCategories] = useState<CategoryBackend[]>();
 
   const handleClickOpen = (id: String) => {
     setOpen(id);
@@ -52,8 +61,30 @@ function ManageProductsView() {
       .catch((err) => console.log(err));
   };
 
+  const updateCategories = async () => {
+    let res = await getCategoriesFromBackend();
+    let data = await res.json();
+    setCategories(data);
+  };
+
+  const getCategoryTitleById = (id: String) => {
+    if (categories) {
+      let index = 0;
+      for (let i = 0; i < categories.length; i++) {
+        if (categories[index]._id === id) {
+          index = i;
+        }
+      }
+
+      return categories[index].title || "";
+    }
+
+    return "";
+  };
+
   useEffect(() => {
     getProductFromBackend();
+    updateCategories();
   }, []);
 
   return (
@@ -92,8 +123,59 @@ function ManageProductsView() {
               <Typography>{item.title}</Typography>
             </AccordionSummary>
             <AccordionDetails>
+              <div className="wrapEvenly">
+                {item.images.map((item: String, i: number) => {
+                  return (
+                    <div key={i}>
+                      <Typography>{`Bild ${i + 1}`}</Typography>
+                      <img
+                        src={getImageUrl(item)}
+                        alt={`bild ${i + 1}`}
+                        width="200px"
+                      />
+                    </div>
+                  );
+                })}
+              </div>
+              <div className="wrapEvenly">
+                {item.info.map((item: String, i: number) => {
+                  return (
+                    <div key={i} className="info">
+                      <Typography>
+                        Info{i + 1}:
+                        <br />
+                        {item}
+                      </Typography>
+                    </div>
+                  );
+                })}
+              </div>
+
+              <br />
               <Typography>Lång Info</Typography>
               <Typography>{item.longInfo}</Typography>
+              <br />
+              <Typography>Kategorier</Typography>
+              {item.category.map((item: String, i: number) => {
+                return (
+                  <Typography key={i}>{getCategoryTitleById(item)}</Typography>
+                );
+              })}
+              <br />
+              <div className="wrapEvenly">
+                {item.specs.map((item: Specs, i: number) => {
+                  return (
+                    <div key={i} className="specs">
+                      <Typography>{item.spectitle}</Typography>
+                      <Typography>{item.spec}</Typography>
+                    </div>
+                  );
+                })}
+              </div>
+              <div className="stockAndPrice">
+                <Typography>Produkter i lager: {item.stock}</Typography>
+                <Typography>Pris: {item.price} :-</Typography>
+              </div>
               <ButtonGroup>
                 <Button
                   onClick={() => handleClickOpen(item._id)}
