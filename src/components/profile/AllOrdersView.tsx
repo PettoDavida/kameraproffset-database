c563f6b78910eed3c644dee6cb96211582f2074d
PettoDavida <maximilian.widman@gmail.com>
2022-06-03 13:59:26 +0200

    changed design and added all info from order to be visible. also added user email in order on frontend so admin can see who placed order

--- src/components/profile/AllOrdersView.tsx
+++ src/components/profile/AllOrdersView.tsx
@@ -6,37 +6,19 @@ import {
   AccordionDetails,
   Button,
 } from "@mui/material";
+import { DateTime } from "luxon";
 import { useEffect, useState } from "react";
-import { ProductBackend } from "../../utils/backend";
+import {
+  getImageUrl,
+  getUserById,
+  OrderBackend,
+  ProductBackend,
+  UserBackend,
+} from "../../utils/backend";
 import { getLoginToken } from "../../utils/token";
 
-interface Address {
-  street: String;
-  zipcode: Number;
-  city: String;
-  firstName: String;
-  lastName: String;
-}
-
-interface Delivery {
-  title: String;
-  price: Number;
-  info: String;
-  expectedArrival: Date;
-  image: String;
-}
-
-interface Order {
-  _id: String;
-  userID: String;
-  products: ProductBackend[];
-  deliveryAddress: Address;
-  deliveryMethod: Delivery;
-  sent: Boolean;
-}
-
 export default function AllOrdersView() {
-  const [orders, setOrders] = useState<Order[]>([]);
+  const [orders, setOrders] = useState<OrderBackend[]>([]);
 
   const getOrdersFromBackend = async () => {
     let token = getLoginToken();
@@ -46,35 +28,140 @@ export default function AllOrdersView() {
         Authorization: `Bearer ${token}`,
       },
     };
-    fetch("http://localhost:3000/api/order", headers)
-      .then((res: Response) => {
-        return res.json();
-      })
-      .then((data) => setOrders(data))
-      .catch((err) => console.log(err));
+    let res = await fetch("http://localhost:3000/api/order", headers);
+    let data = await res.json();
+    setOrders(data);
+  };
+
+  const [users, setUsers] = useState<Map<String, UserBackend>>();
+
+  const getUsers = async () => {
+    let userIds = new Set<String>();
+
+    orders.map((order: OrderBackend) => {
+      userIds.add(order.userID);
+    });
+
+    let arr = Array.from(userIds);
+    let users = new Map<String, UserBackend>();
+    for (let id of arr) {
+      let res = await getUserById(id);
+      let user: UserBackend = await res.json();
+      users.set(id, user);
+    }
+
+    setUsers(users);
   };
 
   useEffect(() => {
     getOrdersFromBackend();
   }, []);
 
+  useEffect(() => {
+    getUsers();
+  }, [orders]);
+
   return (
     <div>
-      {orders.map((item: Order, i: number) => (
+      {orders.map((item: OrderBackend, i: number) => (
         <Accordion key={i} sx={{ width: "100%" }}>
           <AccordionSummary
             expandIcon={<ExpandMore />}
             aria-controls="panel1a-content"
             id="panel1a-header"
           >
-            <Typography>{item.userID}</Typography>
+            <div
+              style={{
+                width: "100%",
+                display: "flex",
+                justifyContent: "space-between",
+                flexWrap: "wrap",
+              }}
+            >
+              <Typography>Order #{item._id}</Typography>
+              <Typography>{item.sent ? "Skickad" : "ej skickad"}</Typography>
+              <Typography>
+                {DateTime.fromISO(item.createdAt.toString())
+                  .setLocale("sv")
+                  .toLocaleString(DateTime.DATETIME_SHORT)}
+              </Typography>
+            </div>
           </AccordionSummary>
           <AccordionDetails>
-            <Typography>{item.products[0].title}</Typography>
-            <Typography>
-              Order#{item._id} has
-              {item.sent ? " been sent" : " not been sent"}
-            </Typography>
+            <Typography>Produkter:</Typography>
+            <div className="orderProducts">
+              {item.products.map((item: ProductBackend, i: number) => {
+                // console.log(item);
+
+                return (
+                  <div key={i}>
+                    <Typography>{item.title}</Typography>
+                    <img
+                      src={getImageUrl(item.images[0])}
+                      alt=""
+                      width="200px"
+                    />
+                    <Typography>{item.price} :-</Typography>
+                    <Typography>Kvanitet: {item.quantity}</Typography>
+                  </div>
+                );
+              })}
+            </div>
+            <div
+              style={{
+                width: "100%",
+                display: "flex",
+                justifyContent: "space-evenly",
+                flexWrap: "wrap",
+              }}
+            >
+              <div className="deliveryAddress">
+                <Typography>
+                  Förnamn: {item.deliveryAddress.firstName}
+                </Typography>
+                <Typography>
+                  Efternamn: {item.deliveryAddress.lastName}
+                </Typography>
+                <Typography>
+                  Telefon: {item.deliveryAddress.phoneNumber}
+                </Typography>
+                <Typography>Ort: {item.deliveryAddress.city}</Typography>
+                <Typography>
+                  Leverans Adress: {item.deliveryAddress.street}{" "}
+                  {item.deliveryAddress.zipcode}
+                </Typography>
+              </div>
+              <div className="deliveryOption">
+                <Typography>Leverantör: {item.deliveryOption.title}</Typography>
+                <Typography>Pris: {item.deliveryOption.price} :-</Typography>
+                <Typography>
+                  Uppskattad leveranstid:{" "}
+                  {DateTime.fromISO(
+                    item.deliveryOption.expectedArrival.toString()
+                  )
+                    .setLocale("sv")
+                    .toLocaleString(DateTime.DATETIME_SHORT)}
+                </Typography>
+              </div>
+              <div className="totalpris">
+                <Typography>
+                  Totalpris:
+                  {item.products.reduce<number>(
+                    (acc: number, item: ProductBackend) => {
+                      return acc + item.price * (item.quantity || 0);
+                    },
+                    item.deliveryOption.price
+                  )}{" "}
+                  :-
+                </Typography>
+              </div>
+            </div>
+            <br />
+            <div className="Userthatordered">
+              <Typography>
+                Order placerare: {users?.get(item.userID)?.email}
+              </Typography>
+            </div>
             <Button
               variant="contained"
               color="success"
