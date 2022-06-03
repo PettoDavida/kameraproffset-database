165e39c8b724ea02aabcf0aeb53eec729bce0ed2
PettoDavida <maximilian.widman@gmail.com>
2022-06-03 17:23:39 +0200

    added logout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,16 @@
 import AdminPanelSettingsIcon from "@mui/icons-material/AdminPanelSettings";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import Login from "@mui/icons-material/Login";
-import { Badge } from "@mui/material";
+import { Badge, Button } from "@mui/material";
 import { useContext } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { ShoppingCartContext } from "../contexts/ShoppingCartContext";
 import "../CSS/Header.css";
 
 import "./ShoppingCartPage.tsx";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { getLoginToken } from "../utils/token";
+import { Logout } from "@mui/icons-material";
 
 const theme = createTheme({
   palette: {
@@ -29,18 +30,32 @@ const theme = createTheme({
 
 function Header() {
   const { amountOfProducts } = useContext(ShoppingCartContext);
+  const navigate = useNavigate();
   let token = getLoginToken();
   return (
     <ThemeProvider theme={theme}>
       <header id="header" className="show-products">
         <div className="headerLeft">
           {token != null ? (
-            <Link to="/ProfileOrAdminPage">
-              <AdminPanelSettingsIcon
-                className="icon"
-                sx={{ paddingLeft: "1rem", fontSize: "2rem", opacity: "0" }}
-              />
-            </Link>
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <Link to="/ProfileOrAdminPage">
+                <AdminPanelSettingsIcon
+                  className="icon"
+                  sx={{ paddingLeft: "1rem", fontSize: "2rem", opacity: "0" }}
+                />
+              </Link>
+              <Button
+                onClick={() => {
+                  localStorage.removeItem("loginToken");
+                  navigate("/", { replace: true });
+                  window.location.reload();
+                }}
+              >
+                <Badge className="icon" sx={{ opacity: "0" }} color="secondary">
+                  <Logout sx={{ fontSize: "2rem" }} />
+                </Badge>
+              </Button>
+            </div>
           ) : (
             <div>
               <Link to="/LogIn">
